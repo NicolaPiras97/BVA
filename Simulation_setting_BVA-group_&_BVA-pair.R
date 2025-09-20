24689ff3de17d52d7ac4d6cff1816dbb3681fd60
NicolaPiras97 <npiras1997@gmail.com>
2025-09-20 12:36:29 +0200

    Update Simulation_setting_BVA-group_&_BVA-pair.R

--- Simulation_setting_BVA-group_&_BVA-pair.R
+++ Simulation_setting_BVA-group_&_BVA-pair.R
@@ -1,30 +1,29 @@
-#Code for simulation scenario 1
+#Code for simulation BVA-group and BVA-pair
 library(Rcpp)
 library(RcppEigen)
 library(RcppArmadillo)
-Rcpp::sourceCpp("MCCLC_full_categorical.cpp") #full SEM-Gibbs
-#Rcpp::sourceCpp("MCCLC_hybrid_categorical.cpp") #hybrid SEM-Gibbs
+Rcpp::sourceCpp("MCCLC_categorical_data.cpp") 
 
-L <- 4
+L <- 3
 R <- H <- 2
-K <- 24
-Q <- 36
-ph <- c(0.625,0.375)
-pr <- c(0.25,0.75)
+K <- 30
+Q <- 15
+ph <- c(0.8,0.2)
+pr <- c(0.4,0.6)
 
-pxwz<-matrix(c(0.4,0.3,0.2,0.1,0.15,0.05,0.35,0.45,0.05,0.55,0.15,0.25,0.3,0.05,0.5,0.15),nrow=H*R,ncol=L,byrow=T)
+pxwz<-matrix(c(0.5,0.3,0.2,0.1,0.6,0.3,0.2,0.1,0.7,0.6,0.1,0.3),nrow=H*R,ncol=L,byrow=T)
 
 c1=2
-c2=3
-c3=4
+nk<-10 
+nq<-10
 
-p1<-matrix(c(0.8,0.9,0.3,0.1,0.2,0.1,0.7,0.9),nrow=c1,ncol=L,byrow=T)
-p2<-matrix(c(0.7,0.8,0.2,0.3,0.3,0.2,0.8,0.7),nrow=c1,ncol=L,byrow=T)
-p3<-matrix(c(0.7,0.8,0.1,0.15,0.1,0.05,0.7,0.65,0.2,0.15,0.2,0.2),nrow=c2,ncol=L,byrow=T)
-p4<-matrix(c(0.8,0.1,0.7,0.2,0.05,0.7,0.1,0.7,0.15,0.2,0.2,0.1),nrow=c2,ncol=L,byrow=T)
-p5<-matrix(c(0.65,0.1,0.6,0.2,0.1,0.65,0.1,0.6,0.1,0.1,0.2,0.1,0.15,0.15,0.1,0.1),nrow=c3,ncol=L,byrow=T)
-p6<-matrix(c(0.75,0.2,0.7,0.1,0.05,0.6,0.1,0.7,0.05,0.05,0.1,0.15,0.15,0.15,0.1,0.05),nrow=c3,ncol=L,byrow=T)
+n=K*nk*Q
 
+p1<-matrix(c(0.88888844,0.823529696,0.333333373,0.666665738,0.538462115,0.111111165,0.11111156,0.176470304,0.666666627,0.333334262,0.461537885,0.888888835),nrow=c1,ncol=L*H,byrow=T) 
+p2<-matrix(c(0.7,0.8,0.3,0.3,0.2,0.7),nrow=c1,ncol=L,byrow=T)
+p3<-matrix(c(0.8,0.2,0.9,0.2,0.8,0.1),nrow=c1,ncol=L,byrow=T)
+p4<-matrix(c(0.7,0.3,0.8,0.3,0.7,0.2),nrow=c1,ncol=L,byrow=T)
+p5<-matrix(c(0.4,0.2,0.3,0.6,0.8,0.7),nrow=c1,ncol=L,byrow=T)
 
 S <- 100
 for(s in 1:S){
@@ -38,14 +37,6 @@ components2<-rep(0,Q)
 while(length(which(components2==1))!=round(Q*pr[1])){ #remove for simulation scheme i. (random sampling of memberships of level-2 units)
   components2 <- sample(1:R,prob=pr,size=Q,replace=TRUE)      
 }
-
-
-nk<-13 # scenario 1.1
-nq<-13
-#nk<-24 # scenario 1.2
-#nq<-24
-
-n=K*nk*Q
 
 data <- matrix(nrow=n,ncol=3) 
 data[,1] <- seq(1:n)
@@ -73,8 +64,7 @@ datac<-cbind(data,colh,colr)
 datacc<-datac[order(datac[,4],datac[,5]),]
 data<-datacc[,1:3]
 
-count<-c(length(which(components==1))*length(which(components2==1))*nk,length(which(components==1))*length(which(components2==2))*nk,length(which(components==2))*length(which(components2==1))*nk,length(which(components==2))*length(which(components2==2))*nk)
-
+count<-c(length(which(daticc[,4]==1 & daticc[,5]==1)),length(which(daticc[,4]==1 & daticc[,5]==2)),length(which(daticc[,4]==2 & daticc[,5]==1)),length(which(daticc[,4]==2 & daticc[,5]==2)))
 
 data2<-NULL
 samples2 <- NULL
@@ -83,7 +73,10 @@ for(j in (1:H)){
   for(m in (1:R)){
     samples <- sample(1:L,prob=pxwz[w,],size=count[w],replace=TRUE) 
     for(i in (1:count[w])){
-      data2p = cbind(sample(0:(c1-1),prob=p1[,samples[i]],size=1),sample(0:(c1-1),prob=p2[,samples[i]],size=1),sample(0:(c2-1),prob=p3[,samples[i]],size=1),sample(0:(c2-1),prob=p4[,samples[i]],size=1),sample(0:(c3-1),prob=p5[,samples[i]],size=1),sample(0:(c3-1),prob=p6[,samples[i]],size=1))
+      #Scenario 1
+      dati2p = cbind(sample(0:(c1-1),prob=p1[,samples[i]+(j-1)*L],size=1),sample(0:(c1-1),prob=p2[,samples[i]],size=1),sample(0:(c1-1),prob=p3[,samples[i]],size=1),sample(0:(c1-1),prob=p4[,samples[i]],size=1),sample(0:(c1-1),prob=p5[,samples[i]],size=1))
+      #Scenario 2
+      #dati2p = cbind(sample(0:(c1-1),prob=p1[,samples[i]+(m-1)*L],size=1),sample(0:(c1-1),prob=p2[,samples[i]],size=1),sample(0:(c1-1),prob=p3[,samples[i]],size=1),sample(0:(c1-1),prob=p4[,samples[i]],size=1),sample(0:(c1-1),prob=p5[,samples[i]],size=1))
       data2=rbind(data2,data2p)
     } 
     samples2<-c(samples2,samples)  
